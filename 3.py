e85c99e33b1fb32cb34771e463f9124ad0cc876c
Deelite34 <benegeserit@o2.pl>
2020-10-17 20:24:58 +0200

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,30 @@
+import unittest
+
+"""
+Implements algorithm from:
+https://people.revoledu.com/kardi/tutorial/BasicMath/Prime/Algorithm-PrimeFactor.html#:~:text=Algorithm%20for%20Prime%20Factorization&text=The%20simplest%20algorithm%20to%20find,until%20the%20number%20becomes%201.&text=Thus%20100%20divided%20by%202%20become%2050.
+
+"""
+
+class LargestPrimeFactors(unittest.TestCase):
+
+    def calculate(self, input):
+        N = input
+        p = 2
+        result = []
+        while (N >= p**2):
+            if N % p == 0:
+                result.append(p)
+                N = N / p
+            else:
+                p = p + 1
+        result.append(int(N))
+        return(result)        # list with all prime factors
+
+    def test_calculate(self):  # uses only the largest prime factor from result
+        self.assertEqual(self.calculate(13195)[-1], 29)
+
+
+unittest.main(exit=False)
+largest_p_factor = LargestPrimeFactors()
+print(largest_p_factor.calculate(600851475143))
