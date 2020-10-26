50fb6ac251ab48ca0e9b9b0f7ba679fb79dbf56c
Deelite34 <benegeserit@o2.pl>
2020-10-26 14:24:36 +0100

    Add 7th task

--- 7.py
+++ 7.py
@@ -1,23 +1,34 @@
 def is_prime(num):
     divisors = []
-    for i in range(1, num+1):
+    for i in range(1, num + 1):
         if num % i == 0:
             divisors.append(i)
     if len(divisors) == 2 and 1 in divisors and i in divisors:
         return(True)
     return(False)
 
-def find_x_primes(index):
+
+def find_x_primes_range(start, end):
     result = []
-    for i in range(index):
+    for i in range(start, end):
         if is_prime(i):
             result.append(i)
     return(result)
 
-print(find_x_primes(10))
 
-helper = 10000
-while True:
+def find_xth_prime(number):
+    result = []
+    start_index = 1
+    end_index = 1000
+    while len(result) < number:
+        result += find_x_primes_range(start_index, end_index)
+        start_index += 1000
+        end_index += 1000
+    return(result)
 
-    if len(find_x_primes(10000)) < 10000:
-        print("Retrying...")
+
+index = 1000
+
+print(find_xth_prime(index))
+print(find_xth_prime(index)[100])
+print(len(find_xth_prime(index)))
